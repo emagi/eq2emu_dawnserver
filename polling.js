1ae2f0872bf35dc2e984a60532c65f3bc7f8c339
Emagi <image.emagi@gmail.com>
2024-08-16 06:46:05 -0400

    check certs when polling

--- polling.js
+++ polling.js
@@ -7,8 +7,7 @@ var currentCert = "";
 const fetchStatus = async (url, sslFiles, username, password) => {
 	if(httpsAgent == null || currentCert != sslFiles.cert) {		
 		httpsAgent = new https.Agent({
-		  cert: fs.readFileSync(sslFiles.cert),
-		  rejectUnauthorized: false
+		  cert: fs.readFileSync(sslFiles.cert)
 		})
 	}
   try {
