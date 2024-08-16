384a78a1372ef28fd7d98babd1449ca1a8c8af0a
Emagi <image.emagi@gmail.com>
2024-08-16 17:25:53 -0400

    certs need to be available to the server

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 cd /eq2emu/eq2emu-content/
 git pull
 cp -rf ItemScripts Quests RegionScripts SpawnScripts Spells ZoneScripts /eq2emu/eq2emu/server/
+sudo chown eq2emu:eq2emu /eq2emu/certs/
+sudo chmod 644 /eq2emu/certs/*
