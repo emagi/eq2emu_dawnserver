916a153a5a766523475e7a8c9ecab91fff356b5b
Emagi <image.emagi@gmail.com>
2024-08-18 07:18:14 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ git pull
 cp -rf ItemScripts Quests RegionScripts SpawnScripts Spells ZoneScripts /eq2emu/eq2emu/server/
 sudo chown eq2emu:eq2emu /eq2emu/certs/
 sudo chmod 644 /eq2emu/certs/*
-
+sudo chmod +x compile_source_web.sh
 login_status=$(pidof -x "login")
 world_status=$(pidof -x "eq2world")
 if [ "$login_status" == '' && "$world_status" == '' ]; then
