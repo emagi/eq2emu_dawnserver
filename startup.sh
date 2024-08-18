9c653fad5e54abf076bf8bafb483a4ad608280bf
Emagi <image.emagi@gmail.com>
2024-08-18 07:17:03 -0400

    compile source on startup if login and world are down

--- startup.sh
+++ startup.sh
@@ -3,3 +3,12 @@ git pull
 cp -rf ItemScripts Quests RegionScripts SpawnScripts Spells ZoneScripts /eq2emu/eq2emu/server/
 sudo chown eq2emu:eq2emu /eq2emu/certs/
 sudo chmod 644 /eq2emu/certs/*
+
+login_status=$(pidof -x "login")
+world_status=$(pidof -x "eq2world")
+if [ "$login_status" == '' && "$world_status" == '' ]; then
+	screen -d -m bash -x compile_source_web.sh
+	sleep 5
+fi
+
+cd /eq2emu/eq2emu_dawnserver/
