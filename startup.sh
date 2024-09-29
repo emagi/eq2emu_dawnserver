35283a5e4a96e0bf716faef0e77606fe8711b04b
Emagi <image.emagi@gmail.com>
2024-09-29 08:34:16 -0400

    disable content updates in dawn web server startup (will be a separate script)

--- startup.sh
+++ startup.sh
@@ -1,7 +1,3 @@
-cd /eq2emu/eq2emu-content/
-git reset --hard origin
-git pull
-cp -rf ItemScripts Quests RegionScripts SpawnScripts Spells ZoneScripts /eq2emu/eq2emu/server/
 sudo chown eq2emu:eq2emu /eq2emu/certs/
 sudo chmod 644 /eq2emu/certs/*
 
