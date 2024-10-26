529941faea2bfdf99e087f95edcbccb76c074773
Emagi <image.emagi@gmail.com>
2024-10-26 08:09:37 -0400

    Update update_content_fromweb.sh

--- update_content_fromweb.sh
+++ update_content_fromweb.sh
@@ -1,6 +1,6 @@
 touch /eq2emu/eq2emu_dawnserver/updating_content
 cd /eq2emu/eq2emu-content
-git --reset hard
+git reset --hard
 content_update=$(git pull)
 cp -rf ItemScripts Quests RegionScripts SpawnScripts Spells ZoneScripts /eq2emu/eq2emu/server/
 sudo chmod -R 777 ItemScripts Quests RegionScripts SpawnScripts Spells ZoneScripts # allows eq2emu-editor container to write files
